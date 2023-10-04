c3d9ada6cd8f0d7a215ac250ff5b97eeec6a58f0
Noam-Tal Cohen <noamcohenntc@gmail.com>
2023-10-04 09:45:06 +0300

    update

--- public/app.js
+++ public/app.js
@@ -1,5 +1,18 @@
 var OHH = 
     (o1)=>{
+    var out = [];
+    const key = (o)=>{
+        for(var k in o){
+            out.push(k);
+            key(o[k]);
+        }
+    }
+    OHH.out = ()=>{
+        out = [];
+        key(OHH)
+        return out;
+    }
+
     return (OHH[o1]=(o2)=>{
         return (OHH[o1][o2]=(o3)=>{
             return (OHH[o1][o2][o3]=(o4)=>{
@@ -8,18 +21,11 @@ var OHH =
         })
     });
 };
-const key = (o)=>{
-    for(var k in o){
-        console.log(k);
-        key(o[k]);
-    }
-}
+
+
 
 OHH("x")("Y")({z:true})(4)
 OHH("D")("_")
-console.log(OHH.x.Y[{z:true}][4]);
-console.log(OHH.D._)
-
-key(OHH);
+console.log(OHH.out())
 
 
